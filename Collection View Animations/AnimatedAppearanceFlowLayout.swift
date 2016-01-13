1c3ef12e53f1122f8b39094c777566d7235b6e3a
Melissa <melissa@doubleencore.com>
2016-01-13 15:31:05 -0700

    doesn't completely work yet - needs to be fiddled with

--- Collection View Animations/AnimatedAppearanceFlowLayout.swift
+++ Collection View Animations/AnimatedAppearanceFlowLayout.swift
@@ -10,97 +10,82 @@ import UIKit
 
 class AnimatedAppearanceFlowLayout: UICollectionViewFlowLayout {
 
-    var animator = UIDynamicAnimator()
-
-    var indexPathsToAnimate = [NSIndexPath]()
+    var dynamicAnimator = UIDynamicAnimator()
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        animator = UIDynamicAnimator(collectionViewLayout: self)
+        
+        dynamicAnimator = UIDynamicAnimator(collectionViewLayout: self)
+        
+        scrollDirection = UICollectionViewScrollDirection.Vertical
+        minimumInteritemSpacing = 10.0
+        sectionInset = UIEdgeInsetsMake(0.0, 10.0, 0.0, 10.0)
+        itemSize = CGSizeMake(100.0, 100.0)     // this will vary based on the card
     }
-
+    
     //MARK: - Overrides
-
-//    override func initialLayoutAttributesForAppearingItemAtIndexPath(itemIndexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
-//        guard let attributes = layoutAttributesForItemAtIndexPath(itemIndexPath) else { return nil }
-//
-//        if indexPathsToAnimate.contains(itemIndexPath) {
-//            let center = CGPoint(x: attributes.center.x - 50, y: attributes.center.y)
-//
-//            attributes.alpha = 1.0
-//            attributes.center = center
-//        }
-//
-//        return attributes
-//    }
-
+    
     override func prepareLayout() {
         super.prepareLayout()
-
-        setupAttachmentBehaviors()
-    }
-
-    override func prepareForCollectionViewUpdates(updateItems: [UICollectionViewUpdateItem]) {
-        var indexPaths = [NSIndexPath]()
-
-        for item in updateItems {
-            if item.updateAction == UICollectionUpdateAction.Insert || item.updateAction == UICollectionUpdateAction.Move {
-                if let indexPath = item.indexPathAfterUpdate {
-                    indexPaths.append(indexPath)
-                }
-            }
-        }
-
-        indexPathsToAnimate = indexPaths
-
-        resetLayout()
-
-        //animator.behaviors.forEach { item in
-        indexPathsToAnimate.forEach { indexPath in
-            let item = animator.behaviors[indexPath.item]
-            let behavior = item as? UIAttachmentBehavior
-            if let attributes = behavior?.items.first {
-            attributes.center = CGPoint(x: attributes.center.x - 50, y: attributes.center.y)
-            animator.updateItemUsingCurrentState(attributes)
-            }
-        }
-    }
-
-    override func layoutAttributesForElementsInRect(rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        let attributes = animator.itemsInRect(rect) as? [UICollectionViewLayoutAttributes]
-
-        return attributes
-    }
-
-    override func layoutAttributesForItemAtIndexPath(indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
-        if let attributes = animator.layoutAttributesForCellAtIndexPath(indexPath) {
-            return attributes
-        } else {
-            return super.layoutAttributesForItemAtIndexPath(indexPath)!.copy() as? UICollectionViewLayoutAttributes
-        }
-    }
-
-    //MARK: - Utility
-
-    func resetLayout() {
-        animator.removeAllBehaviors()
-        prepareLayout()
-    }
-
-
-    func setupAttachmentBehaviors() {
+        
         guard let contentSize = collectionView?.contentSize else {return}
         guard let attributes = super.layoutAttributesForElementsInRect(CGRect(x: 0.0, y: 0.0, width: contentSize.width, height: contentSize.height)) else { return }
-
-        if animator.behaviors.count == 0 {
+        
+        if dynamicAnimator.behaviors.count == 0 {
             attributes.forEach {
                 let attachment = UIAttachmentBehavior(item: $0, attachedToAnchor: $0.center)
                 attachment.length = 0.0
                 attachment.damping = 0.8
                 attachment.frequency = 1.0
-
-                self.animator.addBehavior(attachment)
+                
+                dynamicAnimator.addBehavior(attachment)
             }
         }
     }
+    
+    override func layoutAttributesForElementsInRect(rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        return dynamicAnimator.itemsInRect(rect) as? [UICollectionViewLayoutAttributes]
+    }
+    
+    override func layoutAttributesForItemAtIndexPath(indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
+        return dynamicAnimator.layoutAttributesForCellAtIndexPath(indexPath)
+    }
+
+    override func prepareForCollectionViewUpdates(updateItems: [UICollectionViewUpdateItem]) {
+        super.prepareForCollectionViewUpdates(updateItems)
+        
+        var newIndexPaths = [NSIndexPath]()
+        
+        for item in updateItems {
+            if item.updateAction == UICollectionUpdateAction.Insert {
+                if let indexPath = item.indexPathAfterUpdate {
+                    newIndexPaths.append(indexPath)
+                }
+            }
+        }
+        
+        for indexPath in newIndexPaths {
+            addBehaviorAndLayoutAttributesToItemAtIndexPath(indexPath)
+        }
+    }
+    
+    func addBehaviorAndLayoutAttributesToItemAtIndexPath(indexPath: NSIndexPath) {
+        guard let newItemLayoutAttributes = super.layoutAttributesForItemAtIndexPath(indexPath) else { return }
+        
+        let attachmentBehavior = UIAttachmentBehavior(item: newItemLayoutAttributes, attachedToAnchor: newItemLayoutAttributes.center)
+        attachmentBehavior.length = 0.0
+        attachmentBehavior.damping = 0.75
+        attachmentBehavior.frequency = 1.0
+        
+        dynamicAnimator.addBehavior(attachmentBehavior)
+        
+        let offScreenX: CGFloat = collectionView!.bounds.width/2.0
+        let offScreenY: CGFloat = collectionView!.frame.height + 50.0 //50 is half of the card width right now
+        
+        newItemLayoutAttributes.center = CGPoint(x: offScreenX, y: offScreenY)
+        newItemLayoutAttributes.alpha = 1.0
+        
+        dynamicAnimator.updateItemUsingCurrentState(newItemLayoutAttributes)
+    }
+
 }
